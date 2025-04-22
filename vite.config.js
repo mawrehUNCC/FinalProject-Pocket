ebe0703a81b555875be7bfbcb725ddd8486c3809
mawreh <mawreh1714@gmail.com>
2025-04-22 01:32:26 -0400

    live website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  base: '/FinalProject-Pocket/', // 👈 Important for GitHub Pages!
+  plugins: [react()],
 });
